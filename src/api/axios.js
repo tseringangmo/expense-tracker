1409a648bd7bbaa62d52554c06791aee44f4071c
tseringangmo <angmo3124@gmail.com>
2026-07-09 15:08:30 +0530

    fix api base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://expense-tracker-f8yt.onrender.com'
+  baseURL: 'https://expense-tracker-f8yt.onrender.com/api'
 })
 
 API.interceptors.request.use((config) => {
